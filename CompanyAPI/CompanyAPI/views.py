e5c0d68b2d101825a4df9bdcd90968770a983d20
Adityakr49 <adityakumarpilot@gmail.com>
2023-08-21 11:10:29 +0530

    GoodToCommit

---
+++ CompanyAPI/CompanyAPI/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse,JsonResponse
+
+
+
+def home_page(request):
+    print("Home page requested")
+    friends=[
+        'ankit',
+        'ravi',
+        'uttam'
+    ]
+    # return HttpResponse("<h1>This is Home page</h1>")
+    return JsonResponse(friends,safe=False)
